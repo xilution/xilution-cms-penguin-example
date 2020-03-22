953c26fcf7b50a38c68099a55455cfdb1bad8fd7
Todd Brunia <tbrunia@gmail.com>
2020-03-22 10:21:18 -0500

    chore: put npx back

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -3,7 +3,7 @@
 BASE_URL=$1
 
 export TS_NODE_PROJECT='./tests/e2e/tsconfig.json'
-codeceptjs run \
+npx codeceptjs run \
   --config ./codecept.conf.js \
   --override "{ \"helpers\": { \"Puppeteer\": { \"url\": \"$BASE_URL\" } } }" \
   --steps \
--- scripts/run-load-tests.sh
+++ scripts/run-load-tests.sh
@@ -3,11 +3,11 @@
 BASE_URL=$1
 
 mkdir -p ./output/load
-artillery run \
+npx artillery run \
   --config ./artillery.conf.yaml \
   --output ./output/load/report.json \
   --overrides "{ \"config\": { \"target\": \"$BASE_URL\" } }" \
   ./tests/load/scenarios.yaml
-artillery report \
+npx artillery report \
   --output ./output/load/report.html \
   ./output/load/report.json
