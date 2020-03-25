d768a4f912e2db348ef08e772762d8f9befc5172
Todd Brunia <tbrunia@gmail.com>
2020-03-25 08:00:39 -0500

    fix: changed codeceptjs config from js to json

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -4,7 +4,7 @@ BASE_URL=$1
 
 export TS_NODE_PROJECT=./tests/e2e/tsconfig.json
 npx codeceptjs run \
-  --config ./codecept.conf.js \
+  --config ./codecept.json \
   --override "{ \"helpers\": { \"Puppeteer\": { \"url\": \"$BASE_URL\" } } }" \
   --steps \
   --reporter mocha-multi
